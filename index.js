28d8c42b0be8634596791de6efa664b70a6f28ba
Andrew Stahl <andrewstahl96@gmail.com>
2022-05-04 19:05:48 -0400

    commit tree 7d6ec45ded19def5f007684690a666dac2bd26ca

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // code your solution here
+const record = [
+  { year: "2015", result: "L"},
+  { year: "2014", result: "N/A"},
+  { year: "2013", result: "L"}
+  //...
+]
+
+function superbowlWin(array) {
+  const result = array.find(element => element.result === "W");
+  return result === undefined ? undefined : result.year; 
+}
